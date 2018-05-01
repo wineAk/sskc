4f67913c3ec445864995c231db64c4dfaf0e8402
wineAk <akasaka@interpark.co.jp>
2018-05-01 14:47:05 +0900

    [fix] cd ディレクトリが変わっても対応できるよう処理を変更

--- cd/files/script.js
+++ cd/files/script.js
@@ -1,6 +1,11 @@
-var version = $('strong').text();
-$('#qrcode').qrcode({
-	width: 320,
-	height: 320,
-	text: 'http://saas.webcrow.jp/apk/CloudScan_ver'+ version +'.apk'
+$(function(){
+	var version = $('strong').text();
+	var url = location.href;
+	var urlRep = url.replace(/qr\.php/, '');
+	var urlApk = urlRep + 'apk/CloudScan_ver'+ version +'.apk'
+	$('#qrcode').qrcode({
+		width: 320,
+		height: 320,
+		text: urlApk,
+	});
 });
