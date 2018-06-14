b29bd7800e8715c78bf146a8104332c5ebe2a3be
wineAk <akasaka@interpark.co.jp>
2018-06-14 09:37:56 +0900

    [add] api

--- wf/accountCreatingPage/script.js
+++ wf/accountCreatingPage/script.js
@@ -1,9 +1,9 @@
-const information = "[script] 1.1.4";
+const information = "[script] 1.1.5";
 // --------------------
 // 初期設定
 // --------------------
 // 各サービスの正規表現
-const classReg = /(lead|sales|teleapo|web|tracking|cti|scan)_.{3}/i;
+const classReg = /(lead|sales|teleapo|web|tracking|cti|scan|api)_.{3}/i;
 // 入力枠のClassを指定しておくこと
 const classObj = {
   "number12" : "password_txt", // パスワード
@@ -13,9 +13,11 @@ const classObj = {
   "number17" : "account_fee",  // アカウント料金
   "number18" : "account_sum",  // アカウント料金 合計
   "number21" : "trial_period", // お試し期間
-  "number38" : "service_fee_sum", // サービス料金 合計
-  "number39" : "saaske_fee_sum",  // サスケ 合計
-  "number40" : "saaske_tax_sum",  // サスケ 合計 税込み
+  // 変動箇所
+  "number40" : "service_fee_sum", // サービス料金 合計
+  "number41" : "saaske_fee_sum",  // サスケ 合計
+  "number42" : "saaske_tax_sum",  // サスケ 合計 税込み
+  // 各種サービス
   "number24" : "lead_fee",  // Lead：金額
   "number25" : "lead_pln",  // Lead：プラン
   "number26" : "sales_fee", // Sales：金額
@@ -30,6 +32,8 @@ const classObj = {
   "number35" : "cti_pln",
   "number36" : "scan_fee",
   "number37" : "scan_pln",
+  "number38" : "api_fee",
+  "number39" : "api_pln",
 };
 // 消費税
 const tax = 1.08;
@@ -37,8 +41,8 @@ const tax = 1.08;
 // パスワード生成
 // --------------------
 function createPassword(){
+  const passNum = Number($('#password input[name="num"]').val());
   let n = 26; // ランダム調整
-  let passNum = Number($('#password input[name="num"]').val());
   let randomString = "";
   let baseString = "abcdefghijklmnopqrstuvwxyz";
   // 数字も使用なら追加
@@ -63,16 +67,16 @@ function createPassword(){
 // http://qiita.com/Slowh/items/5c0b4c16a68f2a29f7e3
 // --------------------
 function getAddMonthDate(year, month, day){
-  let addMonth = month + 1; // 翌月
+  const addMonth = month + 1; // 翌月
   let endDate = getEndOfMonth(year, addMonth); // 翌月の最終日
   if(day > endDate){day = endDate;} // 「今日の日付(31日)」が「翌月の最終日(28日)」より大きいとき、最終日を指定する
-  let addMonthDate = new Date(year, addMonth-1, day); // 計算した結果をnew Dateの形式へ変換
-  let addMonthDateStr = addMonthDate.toLocaleDateString(); // new Dateをyyyy/m/dへ変換
+  const addMonthDate = new Date(year, addMonth-1, day); // 計算した結果をnew Dateの形式へ変換
+  const addMonthDateStr = addMonthDate.toLocaleDateString(); // new Dateをyyyy/m/dへ変換
   return addMonthDateStr;
 }
 //今月の月末日を取得 (翌月の0日目=今月の末日)
 function getEndOfMonth(year, addMonth){
-  let endDate = new Date(year, addMonth, 0);
+  const endDate = new Date(year, addMonth, 0);
   return endDate.getDate();
 }
 // --------------------
@@ -80,33 +84,34 @@ function getEndOfMonth(year, addMonth){
 // --------------------
 function autoCalculation(){
   //アカウント数の処理
-  let paidAccount = Number($('.account_paid input[type="text"]').val());
-  let freeAccount = Number($('.account_free input[type="text"]').val());
+  const paidAccount = Number($('.account_paid input[type="text"]').val());
+  const freeAccount = Number($('.account_free input[type="text"]').val());
   $('.account_num input[type="text"]').val(paidAccount + freeAccount);
   //アカウント金額の処理
-  let feeAccount = Number($('.account_fee input[type="text"]').val());
+  const feeAccount = Number($('.account_fee input[type="text"]').val());
   $('.account_sum input[type="text"]').val(paidAccount * feeAccount);
   //各サービス金額の処理
-  let feeLead     = Number($('.lead_fee input[type="text"]').val());
-  let feeSales    = Number($('.sales_fee input[type="text"]').val());
-  let feeTeleapo  = Number($('.teleapo_fee input[type="text"]').val());
-  let feeWeb      = Number($('.web_fee input[type="text"]').val());
-  let feeTracking = Number($('.tracking_fee input[type="text"]').val());
-  let feeCti      = Number($('.cti_fee input[type="text"]').val());
-  let feeScan     = Number($('.scan_fee input[type="text"]').val());
-  $('.service_fee_sum input[type="text"]').val(feeLead + feeSales + feeTeleapo + feeWeb + feeTracking + feeCti + feeScan);
+  const feeLead     = Number($('.lead_fee input[type="text"]').val());
+  const feeSales    = Number($('.sales_fee input[type="text"]').val());
+  const feeTeleapo  = Number($('.teleapo_fee input[type="text"]').val());
+  const feeWeb      = Number($('.web_fee input[type="text"]').val());
+  const feeTracking = Number($('.tracking_fee input[type="text"]').val());
+  const feeCti      = Number($('.cti_fee input[type="text"]').val());
+  const feeScan     = Number($('.scan_fee input[type="text"]').val());
+  const feeApi     = Number($('.api_fee input[type="text"]').val());
+  $('.service_fee_sum input[type="text"]').val(feeLead + feeSales + feeTeleapo + feeWeb + feeTracking + feeCti + feeScan + feeApi);
   //サスケ 合計
-  let account = Number($('.account_sum input[type="text"]').val());
-  let service = Number($('.service_fee_sum input[type="text"]').val());
+  const account = Number($('.account_sum input[type="text"]').val());
+  const service = Number($('.service_fee_sum input[type="text"]').val());
   $('.saaske_fee_sum input[type="text"]').val(account + service);
   // サスケ 合計 税込み
-  let feeSaaske = Number($('.saaske_fee_sum input[type="text"]').val());
-  let feeSaaskeTax = taxCalculation(feeSaaske);
+  const feeSaaske = Number($('.saaske_fee_sum input[type="text"]').val());
+  const feeSaaskeTax = taxCalculation(feeSaaske);
   $('.saaske_tax_sum input[type="text"]').val(feeSaaskeTax);
 }
 // 消費税計算 (切り捨てfloor, 切り上げceil, 四捨五入round)
 function taxCalculation(price){
-  let priceTax = price * tax;
+  const priceTax = price * tax;
   return Math.floor(priceTax);
 }
 // --------------------
@@ -114,17 +119,17 @@ function taxCalculation(price){
 // --------------------
 $(function() {
   console.info(information);
-  let url = location.href;
-  let urlReg = /secure-link\.jp\/wf\/index\.cgi/i;
+  const url = location.href;
+  const urlReg = /secure-link\.jp\/wf\/index\.cgi/i;
   $(".clr").each(function(i){
     // liタグへClass追加
-    let classNum = i + 1;
-    let className = `number${classNum}`; // ナンバリング
-    let classObjName = classObj[className]; // 特定のClassも追加
-    let thisInput = $(this).find('input[type="text"]').val();
-    let thisText = $(this).children("div").text();
-    let trgtReg = /^(円（税抜き・月額）|件プラン)$/;
-    let classNames = (function(){
+    const classNum = i + 1;
+    const className = `number${classNum}`; // ナンバリング
+    const classObjName = classObj[className]; // 特定のClassも追加
+    const thisInput = $(this).find('input[type="text"]').val();
+    const thisText = $(this).children("div").text();
+    const trgtReg = /^(円（税抜き・月額）|件プラン)$/;
+    const classNames = (function(){
       // 確認画面でtrgtRegと完全一致したとき
       if(trgtReg.test(thisText) && urlReg.test(url)) return `${className} ${classObjName} display_none_imp`;
       // 入力画面でclassObjにサービス名が含まれ、金額・プラン記入済みのとき
@@ -144,23 +149,23 @@ $(function() {
   });
   // パスワードボタン
   $('#password input[type="button"]').click(function(){
-    let pass = createPassword();
+    const pass = createPassword();
     $('.password_txt input[type="text"]').eq(0).val(pass);
   });
   // 1ヶ月後ボタン
   $(".next_month").click(function(){
-    let dt = new Date();
-    let nowYear = dt.getFullYear();
-    let nowMonth = dt.getMonth()+1;
-    let nowDay = dt.getDate();
-    let nextYMD = getAddMonthDate(nowYear, nowMonth, nowDay);
+    const dt = new Date();
+    const nowYear = dt.getFullYear();
+    const nowMonth = dt.getMonth()+1;
+    const nowDay = dt.getDate();
+    const nextYMD = getAddMonthDate(nowYear, nowMonth, nowDay);
     $('.trial_period input[type="text"]').val(nextYMD);
   });
   // 各製品ボタン
   $(".service_button").click(function() {
-    let id = $(this).attr("id");
-    let targetPrnt = $(`.${id}_fee, .${id}_pln`);
-    let targetChld = $(`.${id}_fee input[type="text"], .${id}_pln input[type="text"]`);
+    const id = $(this).attr("id");
+    const targetPrnt = $(`.${id}_fee, .${id}_pln`);
+    const targetChld = $(`.${id}_fee input[type="text"], .${id}_pln input[type="text"]`);
     if(targetPrnt.hasClass("clicked")){ // 閉じるとき
       targetPrnt.removeClass("clicked");
       targetChld.val("");
