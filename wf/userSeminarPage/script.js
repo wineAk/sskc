7ae85a53be11ce6c6cb4609ab91d9f0671cc297f
wineAk <akasaka@interpark.co.jp>
2018-11-15 12:54:25 +0900

    [fix] Bug Fixed

--- wf/userSeminarPage/script.js
+++ wf/userSeminarPage/script.js
@@ -1,13 +1,15 @@
-// --------------------
-// 各種処理
-// --------------------
-$(function() {
-  $("input").click(function(){
-    if($(this).val() == number) {
-      $(this).prop("checked", false);
-      number = 0;
-    } else {
-      number = $(this).val();
-    }
-  });
+$(function(){
+    // ラジオボタンの処理（Bug Fixed）
+    var obj = {};
+    $(document).on('click', 'input:radio', function() {
+        var value = $(this).val();
+        var name = $(this).attr('name');
+        if(value == obj[name]) {
+            $(this).prop('checked', false);
+            obj[name] = 0;
+        } else {
+            $(this).prop('checked', true);
+            obj[name] = value;
+        }
+    });
 });
