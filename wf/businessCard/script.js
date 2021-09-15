28ba46a444000e5d67d501fe25ad180e440ef786
wineAk <akasaka@interpark.co.jp>
2021-09-15 11:30:41 +0900

    [update]

--- wf/businessCard/script.js
+++ wf/businessCard/script.js
@@ -1,28 +1,18 @@
 $(function() {
   var weFormModal = function() {
-    var match = WEBFORMID;
-    //var inputVal = $('input[type=hidden][name="c"]').val();
-    //if (inputVal) match = inputVal;
-    //var imgMatch = $('body > div > img').attr('src').match(/(wf[0-9]+)/);
-    //if (imgMatch) match = imgMatch[1];
-    //$('head > script').each(function() {
-    //  var src = $(this).attr('src');
-    //  if (!src) return;
-    //  var srcMatch = src.match(/(wf[0-9]+)/);
-    //  if (srcMatch) match = srcMatch[1];
-    //});
-    //if (match === '') return;
-    var url = 'https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=https%3A%2F%2Fsecure-link.jp%2Fwf%2F%3Fc%3D' + match;
+    var href = window.location.href;
+    var wf = href.match(/(wf[0-9]+)/)[0];
+    var url = 'https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=https%3A%2F%2Fsecure-link.jp%2Fwf%2F%3Fc%3D' + wf;
     $('.modal-image').attr('src', url);
     $('.modal').css('display', 'block');
     $('.modal').insertAfter('body');
   };
   weFormModal();
-  $('#anchor_inquiryForm > div > div > div > div > article > form > div > input[type=submit]').val('送信する');
+  $('.submit_btn.row input').val('送信する');
   $('[type="file"]').each(function(i) {
     var id = $(this).attr('id');
     var txt = (i === 0) ? '表面' : '裏面';
-    $(this).attr('accept', 'image/*');
+    $(this).attr('accept', 'image/*'); // */
     $(this).wrap('<label for="' + id +'" class="file-container">');
     $(this).before('<div class="file-ui">名刺の「' + txt + '」を撮る</div>');
     $(this).after('<span>選択されていません</span>');
@@ -35,7 +25,7 @@ $(function() {
   });
   $('.input-card').wrapAll('<div id="input-card-container">');
   $('.input-text').wrapAll('<div id="input-text-container">');
-  $(".card-dl").attr('href', $('.card-image img').attr('src'));
+  //$('.card-dl').attr('href', $('.card-image img').attr('src'));
 
   var required = function (trg) {
     $('#input-card-container input, #input-text-container input').each(function() {
@@ -76,23 +66,24 @@ $(function() {
     }
     reader.readAsDataURL(fileName);
   });
-  $(".card-open .card-title").on("click", function() {
-    $("#tableToggle").stop(true, true).fadeToggle(1000);
-    $(".card-open").css('display', 'none');
-    $(".card-close").css('display', 'block');
+  $('.card-open .card-title').on('click', function() {
+    $('#tableToggle').stop(true, true).fadeToggle(1000);
+    $('.card-open').css('display', 'none');
+    $('.card-close').css('display', 'block');
   });
-  $(".card-close .card-title").on("click", function() {
-    $("#tableToggle").stop(true, true).fadeToggle(500);
-    $(".card-close").css('display', 'none');
-    $(".card-open").css('display', 'block');
+  $('.card-close .card-title').on('click', function() {
+    $('#tableToggle').stop(true, true).fadeToggle(500);
+    $('.card-close').css('display', 'none');
+    $('.card-open').css('display', 'block');
   });
-  $(".card-dl").on("click", function(e) {
+  $('.card-dl').on('click', function(e) {
     var hrefPath = cardBase64;
     var fileName = '名刺画像.jpg';
-    $(e.target).attr({
-      download: fileName,
-      href: hrefPath
-    });
+    var alink = document.createElement('a');
+    alink.href = hrefPath;
+    alink.download = fileName;
+    alink.click();
+    return false;
   });
   $('.modal-close').on('click', function() {
     $('.modal').stop(true, true).fadeToggle(1000);
