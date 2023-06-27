1ab8be42e043d665c7f54a27f614d720e3815a42
Ak <36499525+wineAk@users.noreply.github.com>
2023-06-27 15:25:34 +0900

    [update]

--- tm/errorMailCheck.tampermonkey.user.js
+++ tm/errorMailCheck.tampermonkey.user.js
@@ -15,10 +15,8 @@
     const errorTxt = errorElm.textContent.replace(/\n/g, ' ').replace(/\n/g, ' ').replace(/ +/g, ' ')
     console.log('errorTxt:', errorTxt)
     const errorCause =
-        // 再送系
-        (/Will keep trying until message is \d days old/i.test(errorTxt)) ? '（お知らせ）メッセージを試行し続けます' :
-        (/Message could not be delivered for \d days/i.test(errorTxt)) ? '（お知らせ）メッセージは配信されませんでした' :
         // 拒否されているケース
+        (/550[ -]#?5.1.0/.test(errorTxt)) ? '[550 5.1.0] アドレスが拒否されました' :
         (/550[ -]5.7.26/.test(errorTxt)) ? '[550 5.7.26] セキュリティ上のリスクがありブロックされました' :
         (/550[ -]5.4.1/.test(errorTxt)) ? '[550 5.4.1] 受信者アドレスが拒否されました' :
         (/550[ -]5.7.1/.test(errorTxt)) ? '[550 5.7.1] 中継が拒否されました' :
@@ -34,6 +32,7 @@
         (/mailbox for user is full/i.test(errorTxt)) ? 'メールボックスがいっぱいです' :
         (/Disk quota exceeded/i.test(errorTxt)) ? 'メールボックスがいっぱいです' :
         (/\(expanded from \): temporary failure/i.test(errorTxt)) ? '受信者のサーバーでエラーが発生' :
+        (/Connection timed out with/i.test(errorTxt)) ? '接続がタイムアウトしました' :
         // 名前解決が出来てない？
         (/501[ -]5.6.0/.test(errorTxt)) ? '※ [501 5.6.0] データフォーマットエラー' :
         (/501[ -]unacceptable/.test(errorTxt)) ? '※ [501 unacceptable mail address] 受け入れ不可能なメールアドレス' :
@@ -42,16 +41,21 @@
         (/This user doesn't have a .* account/i.test(errorTxt)) ? '該当アカウントを持っていません' :
         (/\(expanded from \): User unknown/i.test(errorTxt)) ? '不明なユーザー' :
         (/\(expanded from (<.+>)?\): unknown user/i.test(errorTxt)) ? '不明なユーザー' :
-        (/The mail system: user unknown /i.test(errorTxt)) ? '不明なユーザー' :
+        (/The mail system: user unknown/i.test(errorTxt)) ? '不明なユーザー' :
+        (/The mail system: unknown user/i.test(errorTxt)) ? '不明なユーザー' :
         (/Reason: unknown user/i.test(errorTxt)) ? '不明なユーザー' :
         (/550 Unknown user/i.test(errorTxt)) ? '[550] 不明なユーザー' :
         (/550 Invalid recipient/i.test(errorTxt)) ? '[550] 受信者のアドレスが無効化されている' :
         (/525[ -]5.7.13/.test(errorTxt)) ? '[525 5.7.13] 受信者のアドレスが無効化されている' :
-        (/550[ -]5.1.1/.test(errorTxt)) ? '※ [550 5.1.1] ユーザーが存在しない' :
-        (/550[ -]5.1.2/.test(errorTxt)) ? '※ [550 5.1.2] ホストが見つかりません' :
-        (/553[ -]5.3.0/.test(errorTxt)) ? '※ [553 5.3.0] ユーザー不明' :
-        (/554[ -]5.0.0/.test(errorTxt)) ? '※ [554 5.0.0] サービスが利用できません' :
+        (/550[ -]5.1.1/.test(errorTxt)) ? '[550 5.1.1] ユーザーが存在しない' :
+        (/550[ -]5.1.2/.test(errorTxt)) ? '[550 5.1.2] ホストが見つかりません' :
+        (/553[ -]5.3.0/.test(errorTxt)) ? '[553 5.3.0] ユーザー不明' :
+        (/554[ -]5.0.0/.test(errorTxt)) ? '[554 5.0.0] サービスが利用できません' :
+        // 再送系
+        (/Will keep trying until message is \d days old/i.test(errorTxt)) ? '（お知らせ）メッセージを試行し続けます' :
+        (/Message could not be delivered for \d days/i.test(errorTxt)) ? '（お知らせ）メッセージは配信されませんでした' :
         // それ以外
+        (/Reason: Delivery failed/i.test(errorTxt)) ? '配信に失敗しました' :
         ''
     titleElm.textContent = errorCause
 })()
