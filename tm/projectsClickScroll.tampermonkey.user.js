767db0a77bb571ec29e025cb1b8a9c0258f6b607
wineAk <akasaka@interpark.co.jp>
2024-03-01 11:07:22 +0900

    [add]

---
+++ tm/projectsClickScroll.tampermonkey.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         案件クリックでページ上部へスクロール（Sales）
+// @namespace    https://my.saaske.com/
+// @version      0.1
+// @description  none
+// @author       wineAk
+// @match        https://my.saaske.com/*
+// @grant        none
+// ==/UserScript==
+
+(() => {
+  const projectsElms = document.querySelectorAll('.projects_detail')
+  if (projectsElms == null) return
+  const opt = { top: 0, behavior: 'smooth' }
+  projectsElms.forEach(e => e.addEventListener('click', _ => window.scroll(opt)))
+})()
